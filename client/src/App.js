a594253ced6494bdff3156cda56657954422b130
João Henrique <joaoenrique.san@gmail.com>
2022-04-29 17:34:44 -0300

    feat: finish section 7

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import getWeb3 from './getWeb3';
 import './App.css';
 
 class App extends Component {
-  state = {};
+  state = { loaded: false, cost: 0, itemName: 'example_1' };
 
   componentDidMount = async () => {
     try {
@@ -18,20 +18,21 @@ class App extends Component {
 
       // Get the contract instance.
       this.networkId = await this.web3.eth.net.getId();
-      this.itemManager = new web3.eth.Contract(
+      this.itemManager = new this.web3.eth.Contract(
         ItemManagerContract.abi,
-        ItemManagerContract.networks[networkId] &&
-          ItemManagerContract.networks[networkId].address
+        ItemManagerContract.networks[this.networkId] &&
+          ItemManagerContract.networks[this.networkId].address
       );
-      this.item = new web3.eth.Contract(
+      this.item = new this.web3.eth.Contract(
         ItemContract.abi,
-        ItemContract.networks[networkId] &&
-          ItemManagerContract.networks[networkId].address
+        ItemContract.networks[this.networkId] &&
+          ItemManagerContract.networks[this.networkId].address
       );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.listenToPaymentEvent();
+      this.setState({ loaded: true });
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -41,36 +42,68 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
+  listenToPaymentEvent = () => {
+    let self = this;
+    this.itemManager.events.SupplyChainStep().on('data', async function (evt) {
+      console.log(evt);
+      let itemObj = await self.itemManager.methods
+        .items(evt.returnValues._itemIndex)
+        .call();
+      console.log(itemObj);
+      alert('Item ' + itemObj._identifier + ' was paid, deliver it now');
+    });
   };
 
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = async () => {
+    const { cost, itemName } = this.state;
+    console.log(cost, itemName, this.itemManager);
+    let result = await this.itemManager.methods
+      .createItem(itemName, cost)
+      .send({ from: this.accounts[0] });
+    console.log(result);
+    console.log('AAAAAAA');
+    alert(
+      'Send' +
+        cost +
+        'Wei tp ' +
+        result.events.SupplyChainStep.returnValues._itemAddress
+    );
+  };
   render() {
-    if (!this.state.web3) {
+    if (!this.state.loaded) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
       <div className='App'>
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <h1>Event Trigger / Supply Chain Example</h1>
+        <h2>Items</h2>
+        <h2>Add Items</h2>
+        Const in wei:{' '}
+        <input
+          type='text'
+          name='cost'
+          value={this.state.cost}
+          onChange={this.handleInputChange}
+        />
+        Item Identifier:{' '}
+        <input
+          type='text'
+          name='itemName'
+          value={this.state.itemName}
+          onChange={this.handleInputChange}
+        />
+        <button type='button' onClick={this.handleSubmit}>
+          Create new Item
+        </button>
       </div>
     );
   }
--- test/simplestorage.js
+++ test/simplestorage.js
@@ -1,15 +1,17 @@
-const SimpleStorage = artifacts.require("./SimpleStorage.sol");
+const ItemManager = artifacts.require('./ItemManager.sol');
 
-contract("SimpleStorage", accounts => {
-  it("...should store the value 89.", async () => {
-    const simpleStorageInstance = await SimpleStorage.deployed();
+contract('ItemManager', (accounts) => {
+  it('... should be able to add an Item', async function () {
+    const itemManagerInstance = await ItemManager.deployed();
+    const itemName = 'test1';
+    const itemPrice = 500;
 
-    // Set value of 89
-    await simpleStorageInstance.set(89, { from: accounts[0] });
+    const result = await itemManagerInstance.createItem(itemName, itemPrice, {
+      from: accounts[0],
+    });
+    assert.equal(result.logs[0].args._itemIndex, 0, "It's not the first item");
 
-    // Get stored value
-    const storedData = await simpleStorageInstance.get.call();
-
-    assert.equal(storedData, 89, "The value 89 was not stored.");
+    const item = await itemManagerInstance.items(0);
+    assert.equal(item._identifier, itemName, 'The identifier was different');
   });
 });
