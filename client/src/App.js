eaffe15cc88c3959f5db47a4cdf81037d0369602
João Henrique <joaoenrique.san@gmail.com>
2022-05-03 16:34:32 -0300

    feat: finish project

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,54 @@
 import React, { Component } from 'react';
-import { myToken } from './contracts/MyToken.json';
-import { myTokenSale } from './contracts/MyTokenSale.json';
-import { KycContract } from './contracts/KycContract.json';
+import MyToken from './contracts/MyToken.json';
+import MyTokenSale from './contracts/MyTokenSale.json';
+import KycContract from './contracts/KycContract.json';
 import getWeb3 from './getWeb3';
 
 import './App.css';
 
 class App extends Component {
-  state = {};
+  state = {
+    loaded: false,
+    kycAddress: '0x123...',
+    tokenSaleAddress: null,
+    userTokens: 0,
+  };
 
   componentDidMount = async () => {
     try {
       // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+      this.web3 = await getWeb3();
 
       // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+      this.accounts = await this.web3.eth.getAccounts();
 
       // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address
+      this.networkId = await this.web3.eth.net.getId();
+      this.tokenInstance = new this.web3.eth.Contract(
+        MyToken.abi,
+        MyToken.networks[this.networkId] &&
+          MyToken.networks[this.networkId].address
       );
-
+      this.tokenSaleInstance = new this.web3.eth.Contract(
+        MyTokenSale.abi,
+        MyTokenSale.networks[this.networkId] &&
+          MyTokenSale.networks[this.networkId].address
+      );
+      this.kycInstance = new this.web3.eth.Contract(
+        KycContract.abi,
+        KycContract.networks[this.networkId] &&
+          KycContract.networks[this.networkId].address
+      );
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({});
+      this.listenToTokenTransfer();
+      this.setState(
+        {
+          loaded: true,
+          tokenSaleAddress: MyTokenSale.networks[this.networkId].address,
+        },
+        this.updateUserTokens
+      );
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -37,23 +58,71 @@ class App extends Component {
     }
   };
 
+  updateUserTokens = async () => {
+    let userTokens = await this.tokenInstance.methods
+      .balanceOf(this.accounts[0])
+      .call();
+    this.setState({ userTokens: userTokens });
+  };
+
+  listenToTokenTransfer = async () => {
+    this.tokenInstance.events
+      .Transfer({ to: this.accounts[0] })
+      .on('data', this.updateUserTokens);
+  };
+
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleBuyTokens = async () => {
+    await this.tokenSaleInstance.methods.buyTokens(this.accounts[0]).send({
+      from: this.accounts[0],
+      value: this.web3.utils.toWei('1', 'wei'),
+    });
+  };
+
+  handleKycWhitelisting = async () => {
+    await this.kycInstance.methods
+      .setKycCompleted(this.state.kycAddress)
+      .send({ from: this.accounts[0] });
+    alert('KYC for ' + this.state.kycAddress + 'is completed');
+  };
+
   render() {
-    if (!this.state.web3) {
+    if (!this.state.loaded) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
       <div className='App'>
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
+        <h1>StarDucks Cappucino Token Sale!</h1>
+        <p>Get your Tokens today!</p>
+        <h2>Kyc Whitelisting</h2>
+        Address to allow:{' '}
+        <input
+          type='text'
+          name='kycAddress'
+          value={this.state.kycAddress}
+          onChange={this.handleInputChange}
+        />
+        <button type='button' onClick={this.handleKycWhitelisting}>
+          Add to whitelist
+        </button>
+        <h2>Buy Tokens</h2>
         <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
+          If you want to buy tokens, send Wei to this adddress:{' '}
+          {this.state.tokenSaleAddress}
         </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <p>You currently have: {this.state.userTokens} CAPPU tokens</p>
+        <button type='button' onClick={this.handleBuyTokens}>
+          Buy more tokens
+        </button>
       </div>
     );
   }
--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,7 @@
 const path = require('path');
-
+const HDwalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config({ path: './.env' });
+const AccountIndex = 0;
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -7,6 +9,36 @@ module.exports = {
   networks: {
     develop: {
       port: 8545,
+    },
+    ganache_local: {
+      provider: function () {
+        return new HDwalletProvider(
+          process.env.MNEMONIC,
+          'http://127.0.0.1:7545',
+          AccountIndex
+        );
+      },
+      network_id: 5777,
+    },
+    goerli_infura: {
+      provider: function () {
+        return new HDwalletProvider(
+          process.env.MNEMONIC,
+          'https://goerli.infura.io/v3/cd3ded1332ab4b4fb44f682a681a3007',
+          AccountIndex
+        );
+      },
+      network_id: 5,
+    },
+    ropsten_infura: {
+      provider: function () {
+        return new HDwalletProvider(
+          process.env.MNEMONIC,
+          'wss://ropsten.infura.io/ws/v3/cd3ded1332ab4b4fb44f682a681a3007',
+          AccountIndex
+        );
+      },
+      network_id: 3,
     },
   },
   compilers: {
